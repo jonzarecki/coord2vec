6a268ff28f2e9a9b01ff1c1614bd38605b7a0ae2
idanbrus <skandal0>
2019-06-30 12:35:39 +0300

    change line separators + update docker run.sh

--- models/architectures.py
+++ models/architectures.py
@@ -1,17 +1,62 @@
 import torch.nn as nn
 import torchvision.models as models
 
-def rgb_pretrained_resnet50():
-    return models.resnet50(pretrained=True)
 
-def rgb_resnet50():
-    return models.resnet50()
+##########################
+##    Architechtures    ##
+##########################
 
-def resnet50(n_channels):
+def rgb_pretrained_resnet50(output_dim: int) -> nn.Module:
+    net = models.resnet50(pretrained=True)
+    return _change_last_layer(net, output_dim)
+
+
+def rgb_resnet50(output_dim: int) -> nn.Module:
+    net = models.resnet50(pretrained=False)
+    return _change_last_layer(net, output_dim)
+
+
+def resnet50(n_channels: int, output_dim: int) -> nn.Module:
+    """
+    create a resnet-18 with first and last layers to fit the coord2vec
+    Args:
+        n_channels: number of OSM channels in the input
+        output_dim: dimension of the output - based on all the losses we want
+
+    Returns:
+        A nn.Module of the resnet
+    """
     resnet = models.resnet50()
-    resnet_except_first = nn.Sequential(*list(resnet.children())[1:])
-    first_conv = nn.Conv2d(n_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-    return nn.Sequential(first_conv, resnet_except_first)
+    return _change_last_layer(_change_first_layer(resnet, n_channels), output_dim)
+
+
+def resnet18(n_channels: int, output_dim: int) -> nn.Module:
+    """
+    create a resnet-18 with first and last layers to fit the coord2vec
+    Args:
+        n_channels: number of OSM channels in the input
+        output_dim: dimension of the output - based on all the losses we want
+
+    Returns:
+        A nn.Module of the resnet
+    """
+    resnet = models.resnet18()
+    return _change_last_layer(_change_first_layer(resnet, n_channels), output_dim)
+
+
+##########################
+##    util functions    ##
+##########################
+
+def _change_last_layer(net, output_dim):
+    list(net.children())[-1].out_features = output_dim
+    return net
+
+
+def _change_first_layer(net, in_channels):
+    list(net.children())[0].in_channels = in_channels
+    return net
+
 
 if __name__ == '__main__':
     print("Zarecki is ugly")
