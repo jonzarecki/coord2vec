d807e4a9cc98da624026e701f4daa24ca752e747
L-TLV-00445086\Uriel <urielsinger@gmail.com>
2019-07-10 13:40:07 +0300

    java installation

--- project_init.sh
+++ project_init.sh
@@ -8,3 +8,7 @@ conda activate coord2vec
 
 bash ./coord2vec/feature_extraction/osm/initialize_osm_postgres.sh
 bash ./coord2vec/image_extraction/init_tile_servers.sh
+
+sudo apt install openjdk-11-jdk
+java -version
+sudo update-alternatives --config java
