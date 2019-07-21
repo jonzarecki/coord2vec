2a264da2285dbb118f640ef68d889ed3d4521b66
idanbrus <skandal0>
2019-07-21 14:30:24 +0300

    fixed the house price evaluation

--- coord2vec/evaluation/tasks/compare_baselines.py
+++ coord2vec/evaluation/tasks/compare_baselines.py
@@ -5,6 +5,7 @@ from coord2vec.evaluation.tasks import HousePricing
 from coord2vec.models.baselines import *
 from coord2vec.feature_extraction.features_builders import example_features_builder
 
+
 def compare_baselines(task, baselines, **fit_kwargs):
     task_handler = task()
     coords, features, y = task_handler.get_data()
@@ -24,10 +25,11 @@ def compare_baselines(task, baselines, **fit_kwargs):
 
     return scores
 
+
 if __name__ == '__main__':
-    coord2vec = Coord2Vec(example_features_builder, n_channels=3)
-    baselines = [coord2vec.load_trained_model('../../../coord2vec/models/saved_models/first_model.pt'),
-                 Coord2Featrues().load_trained_model('')]
+    coord2vec = Coord2Vec(example_features_builder, n_channels=3).load_trained_model(
+        '../../../coord2vec/models/saved_models/first_model.pt')
+    baselines = [coord2vec, Coord2Featrues().load_trained_model('')]
     fit_kwargs = {'cache_dir': config.CACHE_DIR}
 
     scores = compare_baselines(HousePricing, baselines, **fit_kwargs)
