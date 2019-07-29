1aadd0b70bda24902a63f55968e4396c0784fde5
Jonathan Zarecki <yonilx@gmail.com>
2019-07-29 17:59:30 +0300

    add image update to tensorboard during training

--- coord2vec/models/baselines/coord2vec_model.py
+++ coord2vec/models/baselines/coord2vec_model.py
@@ -1,7 +1,8 @@
 import datetime
 import os
 from typing import List, Tuple
-
+import matplotlib.pyplot as plt
+import random
 import torch
 from sklearn.base import BaseEstimator
 from torch import nn
@@ -123,6 +124,33 @@ class Coord2Vec(BaseEstimator):
                                       global_step=global_step)
                 global_step += 1
 
+            fig = plt.figure(epoch, figsize=(3, 1.5), dpi=500)
+            r = random.randint(0, len(images_batch) - 1)
+
+            im = images_batch[r].cpu().numpy().swapaxes(0, 1).swapaxes(1, 2).astype('int')
+            batch_output = self.model.forward(images_batch)[1]
+
+            plt.axis("off")
+            plt.imshow(im[:, :, 0])
+
+            title_font = {'size': '4', 'color': 'black', 'weight': 'normal',
+                          'verticalalignment': 'bottom', 'wrap': True,
+                          'ha': 'left'}  # Bottom vertical alignment for more space
+            fig.text(0.15, 0.75, f"actual: [{', '.join([str(a) for a in features_batch[r].cpu().numpy()])}]",
+                     **title_font)
+            fig.text(0.15, 0.8, f"predicted: [{', '.join([str(head_out[r].item()) for head_out in batch_output])}]",
+                     **title_font)
+
+            plt.subplot(1, 3, 2)
+            plt.axis("off")
+            plt.imshow(im[:, :, 1])
+
+            plt.subplot(1, 3, 3)
+            plt.axis("off")
+            plt.imshow(im[:, :, 2])
+
+            writer.add_figure(tag="Image epoch example", figure=fig, global_step=global_step)
+
             self.save_trained_model(config.COORD2VEC_DIR_PATH + "/models/saved_models/trained_model.pkl")
         return self.model
 
@@ -152,6 +180,7 @@ class Coord2Vec(BaseEstimator):
             path: path of the saved torch NN
         """
         self.model = self.model.to('cpu')
+        os.makedirs(os.path.dirname(path), exist_ok=True)
 
         torch.save({
             'epoch': self.epoch,
