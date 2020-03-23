12d86d2ae226dc835c9e2a74df1eac8ea3257ae2
Adir Dayan <adir.dayan@mail.huji.ac.il>
2020-03-23 11:38:10 +0200

    make the autoencoder tidy

--- coord2vec/Noam_Adir/Adir/autoencoder.py
+++ coord2vec/Noam_Adir/Adir/autoencoder.py
@@ -1,66 +1,51 @@
-import inspect
-import os
-
-from sklearn.preprocessing import StandardScaler
-import torch
-import torchvision
-from torch import nn
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from torchvision.datasets import MNIST
-from torchvision.utils import save_image
-import pandas as pd
-import sys
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-import pytorch_lightning as pl
-from pytorch_lightning import Trainer
-from pipeline_adir import *
 import pickle
-from feature_dataset import Feature_Dataset
-from pytorch_lightning.loggers import TensorBoardLogger
+
+import pytorch_lightning as pl
+import torch
 import torch.nn.functional as F
+from sklearn.preprocessing import StandardScaler
+from torch import nn
+from torch.utils.data import DataLoader
+
+from base_pipeline import *
+from feature_dataset import Feature_Dataset
+from preprocess import *
 
 
-def save_to_pickle_features_and_coords():
-    coords, features = get_csv_data()
-    pickle_out_coords = open("coords.pickle", "wb")
+def save_to_pickle_features():
+    cleaned_csv_features = extract_and_filter_csv_data([clean_floor_col, clean_constructionTime_col])
+    all_features = extract_geographical_features(cleaned_csv_features)
     pickle_out_features = open("features.pickle", "wb")
-    pickle.dump(coords, pickle_out_coords)
-    pickle.dump(features, pickle_out_features)
-    pickle_out_coords.close()
+    pickle.dump(all_features, pickle_out_features)
     pickle_out_features.close()
 
 
-# save_to_pickle_features_and_coords()
+# save_to_pickle_features()
 
 
-def load_from_pickle_features_and_coords():
-    pickle_in_coords = open("coords.pickle", "rb")
-    coords = pickle.load(pickle_in_coords)
-    pickle_in_coords.close()
+def load_from_pickle_features():
     pickle_in_features = open("features.pickle", "rb")
     features = pickle.load(pickle_in_features)
     # print(features)
     pickle_in_features.close()
-    return coords, features
+    return features
 
 
 class Autoencoder(pl.LightningModule):
     def __init__(self, optimizer=torch.optim.Adam, loss_func=F.mse_loss,
-                 learning_rate=1e-1, weight_decay=0, num_train=10, num_features=10, batch_size=20,
-                 embedding_dim=15):
+                 learning_rate=1e-1, weight_decay=0, num_train=10, batch_size=20,
+                 embedding_dim=15, use_all_data=False):
         super(Autoencoder, self).__init__()
         self.loss_fn = loss_func
         self.num_train = num_train
-        self.num_features = num_features
         self.batch_size = batch_size
         self.embedding_dim = embedding_dim
+        self.use_all_data = use_all_data
+        self.prepare_data()
+        self.num_features = self.X_train.shape[1]
         self.init_layers()
         self.optimizer = optimizer(self.parameters(), lr=learning_rate, weight_decay=weight_decay)
-        self.prepare_data()
+        self.emb_train_list = []
 
     def init_layers(self):
         self.encoder = nn.Sequential(
@@ -71,36 +56,30 @@ class Autoencoder(pl.LightningModule):
             nn.Linear(self.embedding_dim, self.num_features)
         )
 
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+    def forward(self, x, return_emb=False):
+        emb = self.encoder(x)
+        output = self.decoder(emb)
+        return output, emb if return_emb else output
 
     def prepare_data(self):
-        coords, features = load_from_pickle_features_and_coords()  # features contain also x and y
-        features['coord'] = coords
+        features = load_from_pickle_features()
+        if not self.use_all_data:
+            features = features[:self.num_train]
+        X = features.drop(columns=["coord", "coord_id", "totalPrice"]).values.astype(float)
 
-        clean_funcs = [clean_floor_col, clean_constructionTime_col]  # can add function if needed
-        cleaned_features = generic_clean_col(features, clean_funcs)
-        self.coords = cleaned_features['coord']
-        self.coords = self.coords[:self.num_train]
-
-        X = cleaned_features.drop(columns=['totalPrice', 'coord']).values.astype(float)
-        # todo delete next line to get all the data
-        X = X[: self.num_train, :self.num_features]
-
+        # z-score normalization
         self.scaler = StandardScaler()
         self.scaler.fit(X)
         X = self.scaler.transform(X)
+
+        # targets in auto encoder are the input
         y = X
 
-        # y = cleaned_features['totalPrice'].values
-        self.X_train, self.X_test, self.y_train, self.y_test, self.coords_train, self.coords_test = train_test_split(X,
-                                                                                                                     y,
-                                                                                                                     self.coords)
+        # train_val_test split
+        self.X_train, self.X_test, self.y_train, self.y_test, self.coords_train, self.coords_test = train_test_split(
+            X, y, features['coord'])
         self.X_train, self.X_val, self.y_train, self.y_val, self.coords_train, self.coords_val = train_test_split(
             self.X_train, self.y_train, self.coords_train)
-        print(self.coords_train.shape)
 
     def train_dataloader(self):
         feature_dataset = Feature_Dataset(self.X_train, self.y_train)
@@ -122,34 +101,26 @@ class Autoencoder(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        output = self.forward(x.float())
-        loss = self.loss_fn(output, y.float())
+        y_hat, embedding_code = self.forward(x.float(), return_emb=True)
+        self.emb_train_list.append(embedding_code.cpu())
+        loss = self.loss_fn(y_hat, y.float())
         logs = {'train_loss': loss}
         return {'loss': loss, 'log': logs}
 
+
     def validation_step(self, batch, batch_idx):
         x, y = batch
-        output = self.forward(x.float())
-        loss = self.loss_fn(output, y.float())
-        return {'val_loss': loss}
+        y_hat, embedding_code = self.forward(x.float(), return_emb=True)
+        loss = self.loss_fn(y_hat, y.float())
+        return {'val_loss': loss,
+                'val_emb': embedding_code.cpu()}
 
     def validation_epoch_end(self, outputs):
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        train_emb = torch.stack(self.emb_train_list)
+        self.emb_train_list = []
+        val_emb = torch.stack([x['val_emb'] for x in outputs])
+        print(f'train_emb = {train_emb.shape}')
+        print(f'val_emb = {val_emb.shape}')
         tensorboard_logs = {'val_loss': avg_loss}
         return {'avg_val_loss': avg_loss, 'log': tensorboard_logs}
-
-    def get_data(self, x_or_y, train_val_or_test):
-        if x_or_y == 'X':
-            if train_val_or_test == 'train':
-                return self.X_train
-            elif train_val_or_test == 'val':
-                return self.X_val
-            else:
-                return self.X_test
-        else:
-            if train_val_or_test == 'train':
-                return self.y_train
-            elif train_val_or_test == 'val':
-                return self.y_val
-            else:
-                return self.y_test
--- coord2vec/Noam_Adir/Adir/main.py
+++ coord2vec/Noam_Adir/Adir/main.py
@@ -2,16 +2,11 @@ import numpy as np
 import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
-
+from itertools import product
 from autoencoder import Autoencoder
 
-num_epochs = 150
-batch_size = 100
-learning_rate = 1e-1
-weight_decay = 1e-8
+num_epochs = 1
 num_train = 500
-num_features = 18
-embedding_dim = 15
 
 
 def add_embedding_visualization_tensor_board(model, logger, num_of_vectors=100):
@@ -37,14 +32,27 @@ def add_embedding_visualization_tensor_board(model, logger, num_of_vectors=100):
 
 
 def main():
-    logs_path = "/mnt/adir_logs"
-    comment = f'lr={learning_rate}, weight_decay={weight_decay}, batch_size={batch_size}'
-    logger = TensorBoardLogger(save_dir=logs_path, name=comment)
-    model = Autoencoder(learning_rate=learning_rate, weight_decay=weight_decay, num_train=num_train,
-                        num_features=num_features, batch_size=batch_size, embedding_dim=embedding_dim)
-    trainer = Trainer(max_epochs=num_epochs, logger=logger)
-    trainer.fit(model)
-    add_embedding_visualization_tensor_board(model, logger)
+    logs_path = '/mnt/adir_logs'
+    parameters = {
+        'lr': [1e-1],
+        'weight_decay': [1e-8],
+        'emb_dim': [15],
+        'batch_size': [100],
+    }
+    parameters_values = list(parameters.values())
+    for lr, weight_decay, emb_dim, batch_size in product(*parameters_values):
+        comment = f'lr={lr}, wd={weight_decay}, emb_dim={emb_dim}, bsize={batch_size}'
+        logger = TensorBoardLogger(save_dir=logs_path, name=comment)
+        model = Autoencoder(learning_rate=lr, weight_decay=weight_decay, num_train=num_train,
+                            batch_size=batch_size, embedding_dim=emb_dim, use_all_data=True)
+        trainer = Trainer(max_epochs=num_epochs, logger=logger, gpus=0)
+        trainer.fit(model)
+        add_embedding_visualization_tensor_board(model, logger)
+        # logger.experiment.add_hparams(
+        #     {'learning rate': lr, 'weight decay': weight_decay, 'embedding dim': emb_dim, 'batch size': batch_size},
+        #     {'hparam/accuracy': 10 * i, 'hparam/loss': 10 * i}
+        # )
+
 
 
 main()
